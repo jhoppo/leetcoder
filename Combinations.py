270918fb40c6f7c2d229b1b359f1ecdfef98fe4f
jhoppo <jack.hong@oppo-aed.tw>
2022-04-16 13:23:45 +0800

    test

---
+++ Combinations.py
@@ -0,0 +1,5 @@
+class Solution:
+    def combine(n: int, k: int):
+        candiList = [i+1 for i in range(n)]
+        print(candiList)
+        
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import book_covid19
+import Combinations
 if __name__ == '__main__':
-    res = book_covid19.booking()
-    res
+    res = Combinations.Solution.combine(n=6,k=3)
+    print(res)
