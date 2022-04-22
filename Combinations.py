bfebd8a4eaa94c7c8183e16204e2f332b342b04d
jhoppo <jack.hong@oppo-aed.tw>
2022-04-22 22:01:41 +0800

    upload failed Combinations.py XD

--- Combinations.py
+++ Combinations.py
@@ -1,5 +1,10 @@
 class Solution:
     def combine(n: int, k: int):
-        candiList = [i+1 for i in range(n)]
-        print(candiList)
-        
+        cands = [i+1 for i in n]
+        def cb(i=0, res=[]):
+            if len(res) < k:
+                res.append(cands[i])
+                return cb(i=i+1,res=res)
+            else:
+                return res
+        return cb
